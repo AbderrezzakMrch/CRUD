4e471fc8543e57c97b52cba353449f7090daa86e
Abdou.exe <147605515+AbderrezzakMrch@users.noreply.github.com>
2025-01-28 22:36:32 +0100

    Added front-end files

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from "./Users.jsx";
+import CreateUsers from './CreateUsers.jsx'
+import UpdateUsers from './UpdateUsers.jsx'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users/>}></Route>
+        <Route path='/create' element={<CreateUsers/>}></Route>
+        <Route path='/update' element={<UpdateUsers/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
